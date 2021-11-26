d2ae2221425403cd1e5e3b3cc88123899b1a5acf
kmavillanosa <kmavillanosa@gmail.com>
2021-11-26 08:40:25 +0800

    added styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,15 +4,36 @@ import Image from "next/image";
 import { useState } from "react";
 import Facebook from "../components/Facebook";
 import styles from "../styles/Home.module.css";
+
+import Typist from "react-typist";
+
+import "react-typist/dist/Typist.css"
+
+
 const year = new Date().getFullYear();
+
+const items = [{
+  message: <small>See my background</small>,
+  url: "https://kim-avillanosa.gitbook.io/portfolio",
+  title: "Portfolio",
+  logo: "/portfolio_link.png"
+},
+{
+  message: <small>Connect with me via <span><strong>Linkedin</strong></span></small>,
+  url: "https://www.linkedin.com/in/kmavillanosa",
+  title: "Linkedin",
+  logo: "/linkedin.png"
+}
+  , {
+  message: <small>Follow me on <span><strong>Github</strong></span></small>,
+  url: "https://github.com/kmavillanosa",
+  title: "Github",
+  logo: "/github.png"
+}]
 
 
 
 const Home: NextPage = () => {
-  const [modalIsOpen, setIsOpen] = useState(false);
-
-
-
 
   return (
     <div className={styles.container}>
@@ -40,77 +61,71 @@ const Home: NextPage = () => {
         />
 
         <h5 className={styles.title}>
-          Kim Cyriel S. Avillanosa
-        </h5>
-        <p className={styles.description}>
-          <code className={styles.code}>Software Engineer</code>
-        </p>
 
-        <small>kmavillanosa@gmail.com</small>
+          <Typist cursor={{
+            show: true,
+            blink: true,
+            element: '|',
+            hideWhenDone: true,
+            hideWhenDoneDelay: 400,
+          }} >
+            Kim Cyriel S. Avillanosa
+          </Typist>
+
+          <p className={styles.description}>
+            <Typist cursor={{
+              show: true,
+              blink: true,
+              element: '|',
+              hideWhenDone: false,
+              hideWhenDoneDelay: 400,
+            }} >
+              <Typist.Delay ms={2000} />
+              <span >Software </span>
+
+              <Typist.Delay ms={500} />
+              <span>Engineer</span>
+            </Typist>
+          </p>
+        </h5>
 
         <div className={styles.grid}>
-          <a
-            href="https://kim-avillanosa.gitbook.io/portfolio"
-            target="_blank" rel="noreferrer"
-            className={styles.card}
-          >
-            <span className={styles.logo}>
-              <Image
-                src="/portfolio_link.png"
-                alt="Portfolio"
-                width={40}
-                height={40}
-              />
-            </span>
-            <h2>Portfolio</h2>
-            <small>Find more about me</small>
-          </a>
-          <a
-            href="https://www.linkedin.com/in/kmavillanosa"
-            target="_blank" rel="noreferrer"
-            className={styles.card}
-          >
-            <span className={styles.logo}>
-              <Image
-                src="/linkedin.png"
-                alt="Linkedin"
-                width={40}
-                height={40}
-              />
-            </span>
-            <h2>Linkedin</h2>
-            <small>Connect with me via <span><strong>Linkedin</strong></span></small>
-          </a>
-          <a
-            href="https://web.facebook.com/kmavillanosa999"
-            target="_blank" rel="noreferrer"
-            className={styles.card}
-          >
-            <span className={styles.logo}>
-              <Image
-                src="/facebook.svg"
-                alt="Facebook"
-                width={40}
-                height={40}
-              />
-            </span>
-            <h2>Facebook</h2>
-            <small>Connect with me via <span><strong>Facebook</strong></span></small>
-          </a>
+          {items.map((item, idx) => {
+            return <a
+              href={item.url}
+              target="_blank" rel="noreferrer"
+              className={styles.card}
+            >
+              <span className={styles.logo}>
+                <Image
+                  src={item.logo}
+                  alt={item.title}
+                  width={40}
+                  height={40}
+                />
+              </span>
+              <h2>{item.title}</h2>
+              {item.message}
+            </a>
+          })}
+        </div>
+
+        <div className={styles.qrbox}>
+          <Image
+            src="/qr.png"
+            alt="scan me"
+            width={60}
+            height={60}
+          />
         </div>
 
       </main>
 
       <footer className={styles.footer}>
-        <small>Copyright © Kim Cyriel S. Avillanosa {year}</small>
-        <div className={styles.qrbox}>
-          <Image
-            src="/qr.png"
-            alt="Portfolio"
-            width={80}
-            height={80}
-          />
+        <div>
+          <small>Copyright © Kim Cyriel S. Avillanosa {year}</small>
         </div>
+
       </footer>
 
       <Facebook />
